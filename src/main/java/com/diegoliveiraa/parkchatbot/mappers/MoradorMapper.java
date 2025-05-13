fb1d895d51b225dd24ba16f40bffdf615df082f1
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-13 18:19:02 -0300

    feat: implements a return to getVaga

--- src/main/java/com/diegoliveiraa/parkchatbot/mappers/MoradorMapper.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/MoradorMapper.java
@@ -1,9 +1,11 @@
 package com.diegoliveiraa.parkchatbot.mappers;
 
 import com.diegoliveiraa.parkchatbot.dtos.MoradorResponseDTO;
+import com.diegoliveiraa.parkchatbot.entitys.Aluguel;
 import com.diegoliveiraa.parkchatbot.entitys.Morador;
 
 import java.util.Collections;
+import java.util.stream.Collectors;
 
 public class MoradorMapper {
     public static MoradorResponseDTO toDTO(Morador morador) {
@@ -14,9 +16,10 @@ public class MoradorMapper {
                 morador.getNome(),
                 morador.getTelefone(),
                 morador.getResidencia(),
-                null,
+                morador.getVaga() != null ? morador.getVaga().getId() : null,
                 Collections.emptyList(),
                 Collections.emptyList()
         );
     }
 }
+
