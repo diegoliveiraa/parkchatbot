a5f496f8d52d03037ad685311aa01a02ff48e0aa
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-13 15:36:22 -0300

    fix: add new parameter

--- src/main/java/com/diegoliveiraa/parkchatbot/dtos/AluguelResponseDTO.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/AluguelResponseDTO.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 
 public record AluguelResponseDTO(
         Vaga vaga,
+        Morador proprietario,
         Morador inquilino,
         LocalDateTime inicio,
         LocalDateTime fim,
