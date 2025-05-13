f2f1cfd880e42fc4a6400ec1cd9972a69bf8adcc
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-13 18:23:38 -0300

    feat: implements mapper to return

--- src/main/java/com/diegoliveiraa/parkchatbot/services/VagaService.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/services/VagaService.java
@@ -1,13 +1,20 @@
 package com.diegoliveiraa.parkchatbot.services;
 
+import com.diegoliveiraa.parkchatbot.dtos.AtribuirProprietarioRequestDTO;
 import com.diegoliveiraa.parkchatbot.dtos.VagaRequestDTO;
 import com.diegoliveiraa.parkchatbot.dtos.VagaResponseDTO;
+import com.diegoliveiraa.parkchatbot.entitys.Morador;
 import com.diegoliveiraa.parkchatbot.entitys.Vaga;
+import com.diegoliveiraa.parkchatbot.mappers.VagaMapper;
 import com.diegoliveiraa.parkchatbot.repositories.VagaRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Service
 public class VagaService {
@@ -15,26 +22,76 @@ public class VagaService {
     @Autowired
     private VagaRepository vagaRepository;
 
-    public Vaga getVaga(UUID id) {
+    @Autowired
+    private MoradadorService moradadorService;
 
-        Vaga vaga = this.vagaRepository.findById(id).orElseThrow(() -> new RuntimeException("Vaga nao encontrada"));
-        return vaga;
+    public VagaResponseDTO createVaga(VagaRequestDTO dto) throws Exception {
+
+        Vaga vaga = new Vaga(dto);
+        vaga.setDataCadastro(LocalDateTime.now());
+
+        if (dto.proprietario() != null) {
+            Morador proprietario = this.moradadorService.getEntidade(dto.proprietario());
+            vaga.setProprietario(proprietario);
+        }
+
+        this.vagaRepository.save(vaga);
+
+        return VagaMapper.toDTO(vaga);
     }
 
-    public VagaResponseDTO vaga(UUID id) {
-        Vaga vaga = this.getVaga(id);
+    public VagaResponseDTO getVaga(UUID uuid){
 
-        return new VagaResponseDTO(
-                vaga.getNumeroVaga(),
-                vaga.getValorMensal(),
-                vaga.isDisponibilidade(),
-                vaga.getProprietario(),
-                vaga.getLocatario(),
-                vaga.getDataAluguel());
+        Vaga vaga = this.vagaRepository.findById(uuid).orElseThrow(()->new RuntimeException("Vaga não encontrada"));
+        return VagaMapper.toDTO(vaga);
     }
 
-    public VagaResponseDTO createVaga(VagaRequestDTO dto) {
+    public List<VagaResponseDTO> getAllVaga() {
 
-        Vaga newVaga = new Vaga(dto);
+        return this.vagaRepository.findAll().stream()
+                .map(VagaMapper::toDTO
+                )
+                .collect(Collectors.toList());
+    }
+
+    public VagaResponseDTO updateVaga(VagaRequestDTO dto) throws Exception {
+
+        Morador proprietario = this.moradadorService.getEntidade(dto.proprietario());
+
+        Vaga vaga = this.vagaRepository.findById(dto.id()).orElseThrow(()->new RuntimeException("Vaga não encontrada"));
+
+        vaga.setNumeroVaga(dto.numeroVaga());
+        vaga.setValorMensal(dto.valorMensal());
+        vaga.setProprietario(proprietario);
+
+        this.vagaRepository.save(vaga);
+
+        return VagaMapper.toDTO(vaga);
+    }
+
+    public void deleteVaga(UUID uuid) {
+
+        Vaga vaga = this.vagaRepository.findById(uuid)
+                .orElseThrow(() -> new EntityNotFoundException("Vaga não encontrado com id: " + uuid));
+
+        this.vagaRepository.delete(vaga);
+    }
+
+    public Vaga getEntidade(UUID uuid) throws Exception {
+        return this.vagaRepository.findById(uuid).orElseThrow(() -> new EntityNotFoundException("Vaga nao encontrado"));
+    }
+
+    public void atribuirProprietario(UUID vagaId, UUID moradorId) throws Exception {
+        Vaga vaga = this.getEntidade(vagaId);
+        Morador morador = this.moradadorService.getEntidade(moradorId);
+
+        if (vaga.getProprietario() != null) {
+            throw new IllegalStateException("Esta vaga ja possui um proprietario");
+        }
+
+        vaga.setProprietario(morador);
+        morador.setVaga(vaga);
+
+        this.vagaRepository.save(vaga);
     }
 }
