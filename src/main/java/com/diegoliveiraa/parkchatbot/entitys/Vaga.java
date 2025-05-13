c33ffd413db3b3734ac4353a0ad0e2680651d2a8
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-12 23:13:31 -0300

    feat: create Vaga entity

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/entitys/Vaga.java
@@ -0,0 +1,33 @@
+package com.diegoliveiraa.parkchatbot.entitys;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.UUID;
+
+@Entity(name = "vaga")
+@Table(name = "tb-vaga")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "vagaId")
+public class Vaga {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID vagaId;
+    private String numeroVaga;
+    private BigDecimal valorMensal;
+    private boolean disponibilidade;
+    private LocalDateTime dataCadastro;
+
+    @OneToOne()
+    @JoinColumn(name = "proprietario_id")
+    private Morador proprietario;
+
+    @OneToMany(mappedBy = "vaga")
+    private List<Aluguel> historicoAlugueis;
+}
