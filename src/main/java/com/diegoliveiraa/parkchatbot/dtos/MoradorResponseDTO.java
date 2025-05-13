46572121a23a65a1ba4395f923e55d8413a0d8ad
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-13 18:19:38 -0300

    refactor: change type of attribute

--- src/main/java/com/diegoliveiraa/parkchatbot/dtos/MoradorResponseDTO.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/MoradorResponseDTO.java
@@ -4,12 +4,13 @@ import com.diegoliveiraa.parkchatbot.entitys.Aluguel;
 import com.diegoliveiraa.parkchatbot.entitys.Vaga;
 
 import java.util.List;
+import java.util.UUID;
 
 public record MoradorResponseDTO(
         String nome,
         String telefone,
         String residencia,
-        Vaga vaga,
+        UUID vagaId,
         List<Aluguel> alugueisComoInquilino,
         List<Aluguel> alugueisComoProprietario) {
 
