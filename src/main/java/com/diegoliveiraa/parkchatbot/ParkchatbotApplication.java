1aa732b1714961def390625e645b581baa7ea916
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-12 23:11:42 -0300

    feat: initial config

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/ParkchatbotApplication.java
@@ -0,0 +1,13 @@
+package com.diegoliveiraa.parkchatbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkchatbotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkchatbotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/diegoliveiraa/parkchatbot/ParkchatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegoliveiraa.parkchatbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkchatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
