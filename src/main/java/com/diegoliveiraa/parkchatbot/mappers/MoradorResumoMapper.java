96355f58f78e83dd2fc0ef554d9ef947356a7dad
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-13 18:20:04 -0300

    feat: implements MoradorResumoMapper

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/MoradorResumoMapper.java
@@ -0,0 +1,20 @@
+package com.diegoliveiraa.parkchatbot.mappers;
+
+import com.diegoliveiraa.parkchatbot.dtos.MoradorResumoDTO;
+import com.diegoliveiraa.parkchatbot.entitys.Morador;
+
+public class MoradorResumoMapper {
+    public static MoradorResumoDTO toDTO(Morador morador) {
+        if (morador == null) {
+            return null;
+        }
+
+        return new MoradorResumoDTO(
+                morador.getId(),
+                morador.getNome(),
+                morador.getTelefone(),
+                morador.getResidencia()
+        );
+    }
+}
+
