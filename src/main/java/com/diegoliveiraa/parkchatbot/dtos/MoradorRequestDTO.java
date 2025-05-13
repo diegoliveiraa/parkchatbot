e721d09a40ef0187f1f21babc2d46c245042e05a
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-12 23:14:00 -0300

    feat: create request dto

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/MoradorRequestDTO.java
@@ -0,0 +1,8 @@
+package com.diegoliveiraa.parkchatbot.dtos;
+
+import java.util.List;
+import java.util.UUID;
+
+public record MoradorRequestDTO(UUID id, String nome, String telefone, String residencia, UUID vaga,
+                                List<UUID> vagasAlugadas) {
+}
