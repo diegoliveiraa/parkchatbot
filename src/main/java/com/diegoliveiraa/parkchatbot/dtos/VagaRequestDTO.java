d06640d3bfcf9221f02f301429351b718a20c527
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-13 18:23:09 -0300

    refactor: refactor to right attributes

--- src/main/java/com/diegoliveiraa/parkchatbot/dtos/VagaRequestDTO.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/VagaRequestDTO.java
@@ -2,14 +2,14 @@ package com.diegoliveiraa.parkchatbot.dtos;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.UUID;
 
 public record VagaRequestDTO(
+        UUID id,
         String numeroVaga,
         BigDecimal valorMensal,
-        boolean disponibilidade,
         LocalDateTime dataCadastro,
         UUID proprietario,
-        UUID locatario,
-        LocalDateTime dataAluguel) {
+        List<UUID> historicoAlugueis) {
 }
