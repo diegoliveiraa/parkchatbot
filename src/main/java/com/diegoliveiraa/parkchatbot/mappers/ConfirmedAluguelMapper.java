ffa436f6116fdf6ab4a90ba5009726642d6ac9e4
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 17:06:41 -0300

    feat: implement ConfirmedAluguelMapper

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/ConfirmedAluguelMapper.java
@@ -0,0 +1,23 @@
+package com.diegoliveiraa.parkchatbot.mappers;
+
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.ConfirmedAluguelResponseDTO;
+import com.diegoliveiraa.parkchatbot.dtos.interesse.InteresseResponseDTO;
+import com.diegoliveiraa.parkchatbot.entitys.Aluguel;
+import com.diegoliveiraa.parkchatbot.entitys.Interesse;
+
+public class ConfirmedAluguelMapper {
+    public static ConfirmedAluguelResponseDTO toDTO(Aluguel aluguel){
+        if (aluguel == null) {
+            return null;
+        }
+        return new ConfirmedAluguelResponseDTO(
+                aluguel.getId(),
+                aluguel.getVaga().getNumeroVaga(),
+                aluguel.getProprietario().getNome(),
+                aluguel.getInquilino().getNome(),
+                aluguel.getValorMensal(),
+                aluguel.getInicio(),
+                aluguel.getFim()
+        );
+    }
+}
