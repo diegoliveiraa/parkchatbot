91b267429fb74d04e514a0426bda5aceb92c28da
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 17:06:49 -0300

    feat: implement ConfirmedAluguelResponseDTO

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/aluguel/ConfirmedAluguelResponseDTO.java
@@ -0,0 +1,15 @@
+package com.diegoliveiraa.parkchatbot.dtos.aluguel;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record ConfirmedAluguelResponseDTO(
+        UUID aluguelId,
+        String numeroVaga,
+        String nomeProprietario,
+        String nomeInquilino,
+        BigDecimal valorMensal,
+        LocalDateTime inicio,
+        LocalDateTime fim
+) {}
