13bf9dc76ec57158051f5cdef82eda00d31eeade
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 13:23:42 -0300

    feat: implement AluguelResponseDTO

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/aluguel/AluguelResponseDTO.java
@@ -0,0 +1,18 @@
+package com.diegoliveiraa.parkchatbot.dtos.aluguel;
+
+import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorResumoDTO;
+import com.diegoliveiraa.parkchatbot.dtos.morador.VagaMoradorResponseDTO;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record AluguelResponseDTO(
+        VagaMoradorResponseDTO vaga,
+        MoradorResumoDTO proprietario,
+        MoradorResumoDTO inquilino,
+        BigDecimal valorMensal,
+        LocalDateTime inicio,
+        LocalDateTime fim,
+        boolean ativo
+) {
+}
