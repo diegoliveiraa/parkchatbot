a44dc854e4a8240670e1af011d8970d911c538c1
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 16:36:55 -0300

    feat: implement InteresseResponseDTO dto

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/interesse/InteresseResponseDTO.java
@@ -0,0 +1,13 @@
+package com.diegoliveiraa.parkchatbot.dtos.interesse;
+
+import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorResumoDTO;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+public record InteresseResponseDTO(UUID id,
+                                   UUID aluguelId,
+                                   String numeroVaga,
+                                   MoradorResumoDTO interessado,
+                                   LocalDateTime dataInteresse) {
+}
