471913eb6520c56c5b1de63a585ab1885e54a0ff
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 13:24:56 -0300

    refactor: add valorMensal attribute and implement constructor

--- src/main/java/com/diegoliveiraa/parkchatbot/entitys/Aluguel.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/entitys/Aluguel.java
@@ -1,8 +1,10 @@
 package com.diegoliveiraa.parkchatbot.entitys;
 
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelRequestDTO;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
@@ -25,8 +27,16 @@ public class Aluguel {
     @ManyToOne
     private Morador inquilino;
 
+    private BigDecimal valorMensal;
+
     private LocalDateTime inicio;
     private LocalDateTime fim;
 
     private boolean ativo;
+
+    public Aluguel(AluguelRequestDTO dto) {
+        this.valorMensal = dto.valorMensal();
+        this.inicio = dto.inicio();
+        this.fim = dto.fim();
+    }
 }
