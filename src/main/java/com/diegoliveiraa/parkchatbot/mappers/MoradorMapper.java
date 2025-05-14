eb0891da4fba849d18168b621a3b45010fe98f9d
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 13:21:47 -0300

    refactor: implement VagaMoradorMapper

--- src/main/java/com/diegoliveiraa/parkchatbot/mappers/MoradorMapper.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/MoradorMapper.java
@@ -1,11 +1,9 @@
 package com.diegoliveiraa.parkchatbot.mappers;
 
-import com.diegoliveiraa.parkchatbot.dtos.MoradorResponseDTO;
-import com.diegoliveiraa.parkchatbot.entitys.Aluguel;
+import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorResponseDTO;
 import com.diegoliveiraa.parkchatbot.entitys.Morador;
 
 import java.util.Collections;
-import java.util.stream.Collectors;
 
 public class MoradorMapper {
     public static MoradorResponseDTO toDTO(Morador morador) {
@@ -16,7 +14,7 @@ public class MoradorMapper {
                 morador.getNome(),
                 morador.getTelefone(),
                 morador.getResidencia(),
-                morador.getVaga() != null ? morador.getVaga().getId() : null,
+                VagaMoradorMapper.toDo(morador.getVaga()),
                 Collections.emptyList(),
                 Collections.emptyList()
         );
