b13613b95b301584869ae4d05350be3b85c957d7
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 15:08:48 -0300

    feat: create getAllAluguel Get method

--- src/main/java/com/diegoliveiraa/parkchatbot/controllers/AluguelController.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/controllers/AluguelController.java
@@ -6,10 +6,9 @@ import com.diegoliveiraa.parkchatbot.services.AluguelService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/aluguel")
@@ -21,5 +20,10 @@ public class AluguelController {
     public ResponseEntity<AluguelResponseDTO> createAluguel(@RequestBody AluguelRequestDTO dto) throws Exception {
         AluguelResponseDTO responseDTO = this.aluguelService.createAluguel(dto);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+    @GetMapping
+    public ResponseEntity<List<AluguelResponseDTO>> getAllAlugueis(){
+        List<AluguelResponseDTO> responseDTO = this.aluguelService.getAllAluguel();
+        return new ResponseEntity<>(responseDTO,HttpStatus.OK);
     }
 }
