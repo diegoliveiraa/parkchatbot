5f84eae8e5509d159dfafb55ce06d2d1ecca6bc5
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 13:36:14 -0300

    feat: create createAluguel Post method

--- src/main/java/com/diegoliveiraa/parkchatbot/controllers/AluguelController.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/controllers/AluguelController.java
@@ -1,9 +1,25 @@
 package com.diegoliveiraa.parkchatbot.controllers;
 
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelRequestDTO;
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResponseDTO;
+import com.diegoliveiraa.parkchatbot.services.AluguelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/aluguel")
 public class AluguelController {
+    @Autowired
+    private AluguelService aluguelService;
+
+    @PostMapping
+    public ResponseEntity<AluguelResponseDTO> createAluguel(@RequestBody AluguelRequestDTO dto) throws Exception {
+        AluguelResponseDTO responseDTO = this.aluguelService.createAluguel(dto);
+        return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
 }
