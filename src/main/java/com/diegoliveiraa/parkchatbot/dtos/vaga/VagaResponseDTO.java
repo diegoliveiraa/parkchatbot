ab3271f618616ea88bd9c068758e26154f7bebed
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 13:13:20 -0300

    fix: remove valorMensal param

--- src/main/java/com/diegoliveiraa/parkchatbot/dtos/vaga/VagaResponseDTO.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/vaga/VagaResponseDTO.java
@@ -1,4 +1,7 @@
-package com.diegoliveiraa.parkchatbot.dtos;
+package com.diegoliveiraa.parkchatbot.dtos.vaga;
+
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResponseDTO;
+import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorResumoDTO;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -6,7 +9,6 @@ import java.util.List;
 
 public record VagaResponseDTO(
         String numeroVaga,
-        BigDecimal valorMensal,
         LocalDateTime dataCadastro,
         MoradorResumoDTO proprietario,
         List<AluguelResponseDTO> historicoAlugueis) {
