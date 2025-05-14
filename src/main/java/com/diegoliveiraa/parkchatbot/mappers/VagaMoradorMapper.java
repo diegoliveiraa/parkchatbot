c3bcbd394a5cc54144afd5e49cd997db681ec914
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-14 13:14:05 -0300

    feat: implement VagaMoradorMapper

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/VagaMoradorMapper.java
@@ -0,0 +1,13 @@
+package com.diegoliveiraa.parkchatbot.mappers;
+
+import com.diegoliveiraa.parkchatbot.dtos.morador.VagaMoradorResponseDTO;
+import com.diegoliveiraa.parkchatbot.entitys.Vaga;
+
+public class VagaMoradorMapper {
+    public  static VagaMoradorResponseDTO toDo(Vaga vaga){
+        if (vaga == null) {
+            return null;
+        }
+        return new VagaMoradorResponseDTO(vaga.getNumeroVaga());
+    }
+}
