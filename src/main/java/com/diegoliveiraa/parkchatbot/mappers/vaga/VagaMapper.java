0caa5429a48ffbb2c603f6baaec878b0144eb47e
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-15 14:31:20 -0300

    refactor: implement proprietarioList return

--- src/main/java/com/diegoliveiraa/parkchatbot/mappers/vaga/VagaMapper.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/vaga/VagaMapper.java
@@ -1,17 +1,29 @@
 package com.diegoliveiraa.parkchatbot.mappers.vaga;
 
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResumoVagaDTO;
 import com.diegoliveiraa.parkchatbot.dtos.vaga.VagaResumoDTO;
 import com.diegoliveiraa.parkchatbot.entitys.Vaga;
+import com.diegoliveiraa.parkchatbot.mappers.aluguel.AluguelresumoVagaMapper;
 import com.diegoliveiraa.parkchatbot.mappers.morador.MoradorResumoMapper;
 
 import java.util.Collections;
+import java.util.List;
 
 public class VagaMapper {
     public static VagaResumoDTO toDTO(Vaga vaga) {
+        if (vaga == null) {
+            return null;
+        }
+
+        List<AluguelResumoVagaDTO> proprietarioList = vaga.getHistoricoAlugueis() != null
+                ? vaga.getHistoricoAlugueis().stream()
+                .map(AluguelresumoVagaMapper::toDTO)
+                .toList()
+                : List.of();
         return new VagaResumoDTO(
                 vaga.getNumeroVaga(),
                 MoradorResumoMapper.toDTO(vaga.getProprietario()),
-                Collections.emptyList()
+                proprietarioList
         );
     }
 }
