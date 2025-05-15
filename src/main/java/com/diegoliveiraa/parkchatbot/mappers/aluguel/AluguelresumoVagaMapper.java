555999179c8be34035fdaab07b9568a634ad9a1a
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-15 14:28:08 -0300

    feat: implement AluguelresumoVagaMapper

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/aluguel/AluguelresumoVagaMapper.java
@@ -0,0 +1,24 @@
+package com.diegoliveiraa.parkchatbot.mappers.aluguel;
+
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResumoMoradorDTO;
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResumoVagaDTO;
+import com.diegoliveiraa.parkchatbot.entitys.Aluguel;
+
+public class AluguelresumoVagaMapper {
+    public static AluguelResumoVagaDTO toDTO(Aluguel aluguel) {
+        if (aluguel == null) {
+            return null;
+        }
+
+        String inquilino = aluguel.getInquilino() != null
+                ? aluguel.getInquilino().getNome()
+                : null;
+        return new AluguelResumoVagaDTO(
+                aluguel.getId(),
+                aluguel.getVaga().getNumeroVaga(),
+                aluguel.getProprietario().getNome(),
+                inquilino,
+                aluguel.getStatus().toString()
+        );
+    }
+}
