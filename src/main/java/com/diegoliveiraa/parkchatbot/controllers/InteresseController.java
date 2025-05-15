90a3d57b26dcd17530cf3a26f774500a95b90566
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-15 11:48:55 -0300

    feat: implemente cancelInteresse and getAllInteresse

--- src/main/java/com/diegoliveiraa/parkchatbot/controllers/InteresseController.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/controllers/InteresseController.java
@@ -6,10 +6,10 @@ import com.diegoliveiraa.parkchatbot.services.InteresseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/interesse")
@@ -21,5 +21,17 @@ public class InteresseController {
     public ResponseEntity<InteresseResponseDTO> createInterest(@RequestBody InteresseRequestDTO dto) throws Exception {
         InteresseResponseDTO responseDTO = this.interesseService.createInterestAluguel(dto);
         return new ResponseEntity<>(responseDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}/cancelar")
+    public ResponseEntity<InteresseResponseDTO> cancelInteresse(@PathVariable UUID id){
+        InteresseResponseDTO responseDTO = this.interesseService.cancelInteresse(id);
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<InteresseResponseDTO>> getAllInteresse(){
+        List<InteresseResponseDTO> responseDTO = this.interesseService.getAllInteresse();
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
     }
 }
