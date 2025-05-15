15f59467b3c3536f22256a62c7f31db17a5b3e49
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-15 14:32:03 -0300

    refactor: change AluguelResponseDTO to AluguelResumoVagaDTO

--- src/main/java/com/diegoliveiraa/parkchatbot/dtos/vaga/VagaResumoDTO.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/dtos/vaga/VagaResumoDTO.java
@@ -1,11 +1,12 @@
 package com.diegoliveiraa.parkchatbot.dtos.vaga;
 
-import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResponseDTO;
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResumoMoradorDTO;
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResumoVagaDTO;
 import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorResumoDTO;
 
 import java.util.List;
 
 public record VagaResumoDTO(String numeroVaga,
                             MoradorResumoDTO proprietario,
-                            List<AluguelResponseDTO> historicoAlugueis) {
+                            List<AluguelResumoVagaDTO> historicoAlugueis) {
 }
