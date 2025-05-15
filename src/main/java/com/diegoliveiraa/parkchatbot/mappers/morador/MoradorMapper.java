3530c2349d35728f1a2b2e0926ad897c7669177d
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-15 14:30:26 -0300

    refactor: implement inquilinoList and proprietarioList return

--- src/main/java/com/diegoliveiraa/parkchatbot/mappers/morador/MoradorMapper.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/mappers/morador/MoradorMapper.java
@@ -1,23 +1,35 @@
 package com.diegoliveiraa.parkchatbot.mappers.morador;
 
+import com.diegoliveiraa.parkchatbot.dtos.aluguel.AluguelResumoMoradorDTO;
 import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorResponseDTO;
 import com.diegoliveiraa.parkchatbot.entitys.Morador;
+import com.diegoliveiraa.parkchatbot.mappers.aluguel.AluguelResumoMoradorMapper;
 import com.diegoliveiraa.parkchatbot.mappers.vaga.VagaMoradorMapper;
 
-import java.util.Collections;
+import java.util.List;
 
 public class MoradorMapper {
     public static MoradorResponseDTO toDTO(Morador morador) {
         if (morador == null) {
             return null;
         }
+        List<AluguelResumoMoradorDTO> inquilinoList = morador.getAlugueisComoInquilino() != null
+                ? morador.getAlugueisComoInquilino().stream()
+                .map(AluguelResumoMoradorMapper::toDTO)
+                .toList()
+                : List.of();
+        List<AluguelResumoMoradorDTO> proprietarioList = morador.getAlugueisComoProprietario() != null
+                ? morador.getAlugueisComoProprietario().stream()
+                .map(AluguelResumoMoradorMapper::toDTO)
+                .toList()
+                : List.of();
         return new MoradorResponseDTO(
                 morador.getNome(),
                 morador.getTelefone(),
                 morador.getResidencia(),
                 VagaMoradorMapper.toDo(morador.getVaga()),
-                Collections.emptyList(),
-                Collections.emptyList()
+                inquilinoList,
+                proprietarioList
         );
     }
 }
