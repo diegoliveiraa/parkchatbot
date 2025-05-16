c4eab0fc8cf9c1c755fdfb80cbe3ecf566315274
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-16 12:44:08 -0300

    feat: implement findByAluguel method

--- src/main/java/com/diegoliveiraa/parkchatbot/repositories/InteresseRepository.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/repositories/InteresseRepository.java
@@ -1,9 +1,12 @@
 package com.diegoliveiraa.parkchatbot.repositories;
 
+import com.diegoliveiraa.parkchatbot.entitys.Aluguel;
 import com.diegoliveiraa.parkchatbot.entitys.Interesse;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface InteresseRepository extends JpaRepository<Interesse, UUID> {
+    List<Interesse> findByAluguel(UUID aluguelId);
 }
