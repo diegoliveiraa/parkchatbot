14d70459bd335cdacf0c8d158b30cdf24b368a33
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-20 14:26:43 -0300

    feat: implement VagaAlreadyAssignedException

---
+++ src/main/java/com/diegoliveiraa/parkchatbot/exceptions/vaga/VagaAlreadyAssignedException.java
@@ -0,0 +1,7 @@
+package com.diegoliveiraa.parkchatbot.exceptions.vaga;
+
+public class VagaAlreadyAssignedException extends RuntimeException {
+    public VagaAlreadyAssignedException(String message) {
+        super(message);
+    }
+}
