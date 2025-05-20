580280a6fc1cdda663265b57a5d8d2c7ebd58f1b
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-20 14:28:03 -0300

    fix: fix indentation

--- src/main/java/com/diegoliveiraa/parkchatbot/exceptions/vaga/VagaNotFoundException.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/exceptions/vaga/VagaNotFoundException.java
@@ -3,7 +3,7 @@ package com.diegoliveiraa.parkchatbot.exceptions.vaga;
 import com.diegoliveiraa.parkchatbot.exceptions.BusinessException;
 
 public class VagaNotFoundException extends BusinessException {
-    public VagaNotFoundException( ) {
+    public VagaNotFoundException() {
         super("Vaga não encontrada");
     }
 }
