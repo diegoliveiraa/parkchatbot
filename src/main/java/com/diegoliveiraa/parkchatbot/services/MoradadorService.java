a1007f22936630089210547765e0baa1f70bd990
Diego Oliveira <diegoliveiraa1@gmail.com>
2025-05-20 11:54:08 -0300

    feat: implement the validators methods

--- src/main/java/com/diegoliveiraa/parkchatbot/services/MoradadorService.java
+++ src/main/java/com/diegoliveiraa/parkchatbot/services/MoradadorService.java
@@ -3,9 +3,10 @@ package com.diegoliveiraa.parkchatbot.services;
 import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorRequestDTO;
 import com.diegoliveiraa.parkchatbot.dtos.morador.MoradorResponseDTO;
 import com.diegoliveiraa.parkchatbot.entitys.Morador;
+import com.diegoliveiraa.parkchatbot.exceptions.morador.MoradorNotFoundException;
 import com.diegoliveiraa.parkchatbot.mappers.morador.MoradorMapper;
 import com.diegoliveiraa.parkchatbot.repositories.MoradorRepository;
-import jakarta.persistence.EntityNotFoundException;
+import com.diegoliveiraa.parkchatbot.validators.MoradorValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,41 +18,35 @@ import java.util.stream.Collectors;
 public class MoradadorService {
     @Autowired
     private MoradorRepository moradorRepository;
+    @Autowired
+    private MoradorValidator moradorValidator;
 
     public MoradorResponseDTO createMorador(MoradorRequestDTO dto) {
-
+        this.moradorValidator.validateCreate(dto);
         Morador newMorador = new Morador(dto);
-
         this.moradorRepository.save(newMorador);
-
         return MoradorMapper.toDTO(newMorador);
     }
 
     public MoradorResponseDTO updateMorador(MoradorRequestDTO dto) {
-
+        this.moradorValidator.validateUpdate(dto);
         Morador morador = this.moradorRepository.findById(dto.id())
-                .orElseThrow(() -> new EntityNotFoundException("Morador não encontrado com id: " + dto.id()));
-
-
+                .orElseThrow(MoradorNotFoundException::new);
         morador.setNome(dto.nome());
         morador.setTelefone(dto.telefone());
         morador.setResidencia(dto.residencia());
-
         this.moradorRepository.save(morador);
-
         return MoradorMapper.toDTO(morador);
     }
 
     public void deleteMorador(UUID uuid) {
-
+        this.moradorValidator.validateDelete(uuid);
         Morador morador = this.moradorRepository.findById(uuid)
-                .orElseThrow(() -> new EntityNotFoundException("Morador não encontrado com id: " + uuid));
-
+                .orElseThrow(MoradorNotFoundException::new);
         this.moradorRepository.delete(morador);
     }
 
     public List<MoradorResponseDTO> getAllMorador() {
-
         return this.moradorRepository.findAll().stream()
                 .map(MoradorMapper::toDTO
                 )
@@ -59,13 +54,13 @@ public class MoradadorService {
     }
 
     public MoradorResponseDTO getMorador(UUID uuid) throws Exception {
-
-        Morador morador = this.moradorRepository.findById(uuid).orElseThrow(() -> new Exception("Morador nao encontrado"));
-
+        this.moradorValidator.validateGet(uuid);
+        Morador morador = this.moradorRepository.findById(uuid).orElseThrow(MoradorNotFoundException::new);
         return MoradorMapper.toDTO(morador);
     }
 
     public Morador getEntidade(UUID uuid) throws Exception {
-        return this.moradorRepository.findById(uuid).orElseThrow(() -> new Exception("Morador nao encontrado"));
+        this.moradorValidator.validateGet(uuid);
+        return this.moradorRepository.findById(uuid).orElseThrow(MoradorNotFoundException::new);
     }
 }
